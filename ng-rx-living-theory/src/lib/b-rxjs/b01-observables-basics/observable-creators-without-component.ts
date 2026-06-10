3b4027355c4aac8ee309e98227cf179086c80669
Jabi <michal.jabi@gmail.com>
2026-06-10 10:44:12 +0200

    explain(b01): Subject is HOT | Observable is COLD.

--- ng-rx-living-theory/src/lib/b-rxjs/b01-observables-basics/observable-creators-without-component.ts
+++ ng-rx-living-theory/src/lib/b-rxjs/b01-observables-basics/observable-creators-without-component.ts
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 
 // Strumień emutujący liczby od 1 do 5
 // Provider
@@ -15,7 +15,6 @@ const sub2 = numbers$.subscribe((n) => {
 });
 sub2.unsubscribe();
 
-
 // Pełne API (te same emisje i sposób działania?)
 const numbersFullApi$ = new Observable<number>((subscriber) => {
   subscriber.next(1);
@@ -27,10 +26,43 @@ const numbersFullApi$ = new Observable<number>((subscriber) => {
 
 // Consumer 2.1:
 numbersFullApi$.subscribe((n) => {
-  console.log('odbiorca #2.1:', n);
+  console.log(n);
 });
 
-// Consumer 2.2:
-numbersFullApi$.subscribe((n) => {
-  console.log('odbiorca #2.2:', n);
+setTimeout(() => {
+  // Consumer 2.2:
+  numbersFullApi$.subscribe((n) => {
+    console.log(n);
+  });
+}, 3000);
+
+// Pełne API (ale) mogę emitować w dowolnym momencie
+
+// Provider 3:
+const mimicTheNumber$ = new Subject();
+
+
+// ⚔️ Dodaj consumera tutaj 3.1 - i sprawdź jak działa
+
+// Consumer 3.1:
+mimicTheNumber$.subscribe((n) => {
+  console.log(n);
+});
+
+mimicTheNumber$.next(1);
+mimicTheNumber$.next(2);
+mimicTheNumber$.next(3);
+// Consumer 3.2:
+mimicTheNumber$.subscribe((n) => {
+  console.log(n);
+});
+mimicTheNumber$.next(4);
+mimicTheNumber$.next(5);
+
+
+// ⚔️ Dodaj consumera tutaj 3.3 - i sprawdź jak działa
+
+// Consumer 3.3:
+mimicTheNumber$.subscribe((n) => {
+  console.log(n);
 });
