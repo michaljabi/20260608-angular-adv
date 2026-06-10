ae20e687834b1bee2b4def9abf0500d05e556a86
Jabi <michal.jabi@gmail.com>
2026-06-10 11:03:07 +0200

    explain(b01): full API of RxJS

--- ng-rx-living-theory/src/lib/b-rxjs/b01-observables-basics/observable-creators-without-component.ts
+++ ng-rx-living-theory/src/lib/b-rxjs/b01-observables-basics/observable-creators-without-component.ts
@@ -1,12 +1,25 @@
-import { Observable, of, Subject } from 'rxjs';
+// operatory of, from, interval, formEvent -> operatory tzw. KREACYJNE (tworzą observable)
+// również HOT: Subject, BehaviorSubject, AsyncSubject etc.
+import { Observable, of, Subject, NEVER } from 'rxjs';
+
+// operatory tzw. instancyjne (innymi słowy, wrzucone do .pipe() innymi słowy MUSISZ mieć już Observabla instancję!)
+import { map, tap, debounce, debounceTime, reduce, filter } from 'rxjs/operators'; // uwaga teraz wszystkie operatory dostępne w  from 'rxjs';
 
 // Strumień emutujący liczby od 1 do 5
 // Provider
-const numbers$: Observable<number> = of(1, 2, 3, 4, 5);
+const numbers$: Observable<number> = of(1, 8, 2);
 
 // Consumer 1:
-numbers$.subscribe((n) => {
-  console.log('odbiorca #1:', n);
+numbers$.subscribe({
+  next: (n) => {
+    console.log(n);
+  },
+  error: (e) => {
+    console.log(e);
+  },
+  complete: () => {
+    console.log('Completed!');
+  },
 });
 
 // Consumer 2:
@@ -17,30 +30,52 @@ sub2.unsubscribe();
 
 // Pełne API (te same emisje i sposób działania?)
 const numbersFullApi$ = new Observable<number>((subscriber) => {
+  // Tutaj ZABAWKI wszystkie masz - TY!
+  // Ty decydujesz jak zachowuje się ten stumień
+  // subscriber.complete();
   subscriber.next(1);
-  subscriber.next(2);
-  subscriber.next(3);
+
+  // // subscriber.error('JEST BŁĄD')
+
+  // subscriber.next(2);
+  // subscriber.next(3);
   subscriber.next(4);
-  subscriber.next(5);
+
+  setTimeout(() => {
+    subscriber.next(6);
+
+    // // ponieważ mamy WSZYSTKIE zabawki - to ja decyduje czy strumień jest skończony czy nie :)
+    subscriber.complete();
+  }, 2000);
+  // subscriber.next(5);
 });
 
 // Consumer 2.1:
+numbersFullApi$.subscribe({
+  next: (n) => {
+    console.log(n);
+  },
+  error: (e) => {
+    console.log(e);
+  },
+  complete: () => {
+    console.log('Completed!');
+  },
+});
+
+// Consumer 2.2:
 numbersFullApi$.subscribe((n) => {
   console.log(n);
 });
 
-setTimeout(() => {
-  // Consumer 2.2:
-  numbersFullApi$.subscribe((n) => {
-    console.log(n);
-  });
-}, 3000);
+// setTimeout(() => {
+
+// }, 3000);
 
 // Pełne API (ale) mogę emitować w dowolnym momencie
 
 // Provider 3:
 const mimicTheNumber$ = new Subject();
-
 
 // ⚔️ Dodaj consumera tutaj 3.1 - i sprawdź jak działa
 
@@ -58,7 +93,6 @@ mimicTheNumber$.subscribe((n) => {
 });
 mimicTheNumber$.next(4);
 mimicTheNumber$.next(5);
-
 
 // ⚔️ Dodaj consumera tutaj 3.3 - i sprawdź jak działa
 
